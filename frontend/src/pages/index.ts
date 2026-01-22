93a8179ca67f8139c6a39269dd6aa029ef75fc70
debbie <harobedjosh@gmail.com>
2026-01-22 13:12:19 +0100

    Add page exports index

---
+++ frontend/src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Home } from './Home';
+export { default as Circles } from './Circles';
+export { default as CreateCircle } from './CreateCircle';
+export { default as Dashboard } from './Dashboard';
