463d873d8213fdf99fa2877d5fb8415b8d09f8f9
debbie <harobedjosh@gmail.com>
2026-01-22 14:25:38 +0100

    chore(context): update index exports with new contexts

--- frontend/src/context/index.ts
+++ frontend/src/context/index.ts
@@ -1,1 +1,3 @@
 export { WalletProvider, useWallet } from './WalletContext';
+export { CircleProvider, useCircles } from './CircleContext';
+export { ToastProvider, useToastContext } from './ToastContext';
