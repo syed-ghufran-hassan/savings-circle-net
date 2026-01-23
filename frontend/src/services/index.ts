56e8de0278bdc74a5670bb9fedd32969c9f121df
debbie <harobedjosh@gmail.com>
2026-01-23 15:47:22 +0100

    chore(services): update barrel export with blockchain services
    Export all new services: stacks, circles, wallet, escrow, nft

--- frontend/src/services/index.ts
+++ frontend/src/services/index.ts
@@ -1,2 +1,10 @@
+// Service barrel export
 export * from './api';
 export { default as api } from './api';
+
+// Blockchain services
+export * from './stacks';
+export * from './circles';
+export * from './wallet';
+export * from './escrow';
+export * from './nft';
