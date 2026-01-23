3341cfdaa9d40b3e56d99fa4c0da36bb601b3b36
debbie <harobedjosh@gmail.com>
2026-01-23 15:54:34 +0100

    chore(hooks): update barrel export with new blockchain hooks
    Export useCircleDetails, useTransactions, useUserNFTs,
    useMarketplace, useNFTDetails, useBlockchain, useTransactionHistory

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -1,8 +1,14 @@
 export { useContracts } from './useContracts';
-export { useBalance, useTransactions } from './useStacks';
+export { useBalance, useTransactions as useStacksTransactions } from './useStacks';
 export { useTheme } from './useTheme';
 export { useToast } from './useToast';
 export { default as useForm } from './useForm';
 export { default as useLocalStorage } from './useLocalStorage';
 export { useDebounce, useDebouncedCallback } from './useDebounce';
 export { default as useCopyToClipboard } from './useCopyToClipboard';
+
+// New blockchain hooks
+export { useCircleDetails } from './useCircleDetails';
+export { useTransactions } from './useTransactions';
+export { useUserNFTs, useMarketplace, useNFTDetails } from './useNFTs';
+export { useBlockchain, useTransactionHistory } from './useBlockchain';
